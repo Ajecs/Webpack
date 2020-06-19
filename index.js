57d6cec50de100975d15119a7075cf30f9f0a7d4
Nicolas Coriale <nico7coriale@gmail.com>
2020-06-19 15:16:14 -0300

    manejo de modulos con loaders. Url-loader, file-loader

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello World')
+// console.log('Hello World')
+import test1 from './test1'
+import test2 from './test2'
+
+
+// console.log(`${test1}, ${test2}`)
+
+import person from './person.jpg'
+
+// * Es necesario configurar el loader -> file-loader en webpack.config
+
+const img = document.createElement('img')
+img.src = person
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,30 @@
+const path = require('path')
+// Para obtener un path absoluto y asi crear el tipo de salida de webpack
+
 module.exports = {
   entry: './index.js',
-  
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'bundle'),
+  },
+  module: {
+    rules: [
+      {
+      /* 
+        * Las reglas de webpack para gestionar los modulos necesitan de regexp que 
+        * indique el archivo a ser gestionado por 1 o + loaders 
+      */
+        test: /\.jpg$/, 
+        use: {
+          loader: 'url-loader',
+          // Opciones del loader para evitar cargar una img muy grande como base-64
+          // en caso de ser mayor a 1000kb actua file-loader
+          options: {
+            limit: 1000,
+          }
+        }, /* Se utiliza el loader file-loader previamente instalado */
+
+      }
+    ]
+  }
 }
