c3138c80746ce37637ac945cabf2de5a28d707f1
Nicolas Coriale <nico7coriale@gmail.com>
2020-06-19 21:55:08 -0300

    config compuesto, y  generación de config global  segun configs

---
+++ config/webpack.common.js
@@ -0,0 +1,35 @@
+const path = require('path')
+// Para obtener un path absoluto y asi crear el tipo de salida de webpack
+
+var htmlWebpackPlugin = require('html-webpack-plugin')
+
+const config = {
+  entry: './js/index.js',
+  output: {
+    filename: '[hash].bundle.js',
+    path: path.resolve(__dirname, '../', 'bundle'),
+  },
+  module: {
+    rules: [
+      {
+        /*
+         * Las reglas de webpack para gestionar los modulos necesitan de regexp que
+         * indique el archivo a ser gestionado por 1 o + loaders
+         */
+        test: /\.jpg$/,
+        use: {
+          loader: 'url-loader',
+          // Opciones del loader para evitar cargar una img muy grande como base-64
+          // en caso de ser mayor a 1000kb actua file-loader
+          options: {
+            limit: 1000,
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new htmlWebpackPlugin()],
+  // Se debe instanciar un objeto del plugin
+}
+
+module.exports = config
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,14 @@
-const path = require('path')
-// Para obtener un path absoluto y asi crear el tipo de salida de webpack
+const webpackMerge = require('webpack-merge')
+const commonConfig = require('./config/webpack.common') 
 
-var htmlWebpackPlugin = require('html-webpack-plugin')
+
 
 module.exports = (env, argv) => {
   console.log(argv)
   // se puede escuchar los objetos env-> su status ej. o argv que ofrece datos del modo de desarrollo del proyecto
-  return {
-    devtool: argv.mode === 'development' ? 'inline-source-map' : 'source-map',
-    entry: './js/index.js',
-    output: {
-      filename: '[hash].bundle.js',
-      path: path.resolve(__dirname, 'bundle'),
-    },
-    module: {
-      rules: [
-        {
-          /*
-           * Las reglas de webpack para gestionar los modulos necesitan de regexp que
-           * indique el archivo a ser gestionado por 1 o + loaders
-           */
-          test: /\.jpg$/,
-          use: {
-            loader: 'url-loader',
-            // Opciones del loader para evitar cargar una img muy grande como base-64
-            // en caso de ser mayor a 1000kb actua file-loader
-            options: {
-              limit: 1000,
-            },
-          },
-        },
-      ],
-    },
-    plugins: [new htmlWebpackPlugin()],
-    // Se debe instanciar un objeto del plugin
-  }
+  const mode = argv.mode ? argv.mode : 'production'
+  const modeConfig = require(`./config/webpack.${mode}`)
+  // Se obtiene production o development
+  return webpackMerge(commonConfig, modeConfig)
+  // * se obtiene la unión de las dos configuraciones
 }
