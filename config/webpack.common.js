6225265c4ba7757ff6b2ed7d6f5a8079259ed07c
Nicolas Coriale <nico7coriale@gmail.com>
2020-06-24 14:54:21 -0300

    Uso de Vue completo, Code Splitting básico

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -2,15 +2,28 @@ const path = require('path')
 // Para obtener un path absoluto y asi crear el tipo de salida de webpack
 
 const HTMLWebpackPlugin = require('html-webpack-plugin'),
-  miniCssExtractPlugin = require('mini-css-extract-plugin') 
+  miniCssExtractPlugin = require('mini-css-extract-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-
 
 const config = {
   entry: './src/js/index.js',
   output: {
-    filename: '[hash].bundle.js',
+    filename: '[name].[contentHash].bundle.js',
     path: path.resolve(__dirname, '../', 'bundle'),
+  },
+  resolve: {
+    alias: {
+      vue$: 'vue/dist/vue.esm.js',
+      // * Esto permite el uso de la version completa (con compilador) de Vue
+      // Que es instalada como dependencia de desarrollo para ser procesada luego por webpack
+      // Otorgando mas opciones como caching, code-splitting ...
+    },
+  },
+  optimization: {
+    // * Permite el uso de code-splittin a partir del plugin preinstalado 
+    splitChunks: {
+      chunks: 'all',
+    },
   },
   module: {
     rules: [
@@ -25,14 +38,14 @@ const config = {
       },
     ],
   },
-    plugins: [
-      new MiniCssExtractPlugin() ,
-      new HTMLWebpackPlugin({
-        title: 'Webpack desde cero',
-        template: './src/index.html',
-      }),
-    ],
-    // Se debe instanciar un objeto del plugin
-  }
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HTMLWebpackPlugin({
+      title: 'Webpack desde cero',
+      template: './src/index.html',
+    }),
+  ],
+  // Se debe instanciar un objeto del plugin
+}
 
 module.exports = config
